e82a21504460c97d2b7689dfda8876f1044ba887
SheltonTolbert <60357311+SheltonTolbert@users.noreply.github.com>
2020-11-12 20:32:09 -0600

    Tailwind implementation
    installed 3 libraries
    
    Tailwind
    
    npm-run-all - for automatically rebuilding Tailwind on save
    
    Chokidar-cli - notifies npm-run-all when on css changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+    <div className="w-4/5 mx-auto flex p-6 bg-gray-300 mt-10 rounded-lg shadow-md">
+      <div className="ml-6 pt-1">
+        <h1 className="text-2xl text-blue-700 leading-tight">
+          Tailwind is working
+        </h1>
+        <p className="text-base text-gray-700 leading-normal">
+          And it looks so so good 
+          <br/>
+          <br/>
+          <br/>I created some new sctipts in the in the package.json<br/>
+          <br/>
+          <ul>
+          <code><li>&nbsp;&nbsp;&nbsp;&nbsp;'npm start' is now 'npm start:react'</li></code>
+          <br/>
+          <code><li>&nbsp;&nbsp;&nbsp;&nbsp;'npm start' uses npm-run-all to run 'build:tailwind' then runs both 'watch:tailwind' 'start:react' in parallel</li></code>
+          <br/>
+          <code><li>&nbsp;&nbsp;&nbsp;&nbsp;'watch:tailwind' uses chokidar to listen fo rchanges in the css then rebuilds tailwind on change</li></code>
+          <br/>
+          <code><li>&nbsp;&nbsp;&nbsp;&nbsp;'build:tailwind' builds tailwind...</li></code>
+          </ul>
+          <br/>
+          <br/>Don't forget to download <a href='https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss' className='text-purple-700'>IntelliSense</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+  </div>
+  </div>
+
+    
   );
 }
 
