a3436c91b2b31cdb54a0aac81903f3322a223ce4
SheltonTolbert <60357311+SheltonTolbert@users.noreply.github.com>
2020-11-17 06:31:25 -0600

    Asset upload
    Added image, svg and font assets to src/assets

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,35 @@ import React, { FC } from 'react';
 import firebase from './api/firebase';
 
 export const App: FC<{}> = ({}) => {
+//
+// const data = firebase.firestore()
+//                         .collection('issues')
+//                         .where('language', '==', 'en')
+//                         .orderBy('publishedAt', 'desc').limit(5).get().then( snap => {
+//                           console.log('snapshot', snap.docs[0].data());
+//                         });
+  
 
-  const data = firebase.firestore()
-                          .collection('issues')
-                          .where('language', '==', 'en')
-                          .orderBy('publishedAt', 'desc').limit(5).get().then( snap => {
-                            console.log('snapshot', snap.docs[0].data());
-                          });
+
+//var docRef = firebase.firestore().collection("issues").doc("0GYNO9onk7ZG9qR6wtL9");
+
+var docRef = firebase.firestore().collection("issues").doc("0GYNO9onk7ZG9qR6wtL9").collection("articles").doc("8HhSJNPCfhrVnRhkC13V");
+
+docRef.get().then(function(doc) {
+  if (doc.exists) {
+      console.log("Document data:", doc.data());
+  } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+  }
+}).catch(function(error) {
+  console.log("Error getting document:", error);
+});
+
 
   return (
     <div>
+      
     <div className="w-4/5 mx-auto flex p-6 bg-gray-300 mt-10 rounded-lg shadow-md">
       <div className="ml-6 pt-1">
         <h1 className="text-2xl text-blue-700 leading-tight">
