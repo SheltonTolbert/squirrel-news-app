bf74eff8852b5fe30c84571f409db202084e95ff
Oliver Erxleben <oliver.erxleben@me.com>
2020-11-18 22:49:04 +0100

    clean up

--- src/components/Scrollview/Scrollview.tsx
+++ src/components/Scrollview/Scrollview.tsx
@@ -1,10 +1,7 @@
 import React, { CSSProperties, FC, useEffect, useState } from 'react';
 import { Pageview } from '../Pageview/Pageview'
 import { getIssue } from '../../api/firebase';
-import { Article, LANGUAGES,  } from '../../models';
-import { IssuesPage } from '../../pages';
-// Styling functions -------------
-
+import { Article, LANGUAGES, } from '../../models';
 
 // calculate window height based on the screen size, subtract the height of the nav bar (3 rem)
 // This places the scroll bar at the bottom of the page
@@ -12,58 +9,44 @@ const windowHeight = window.screen.height - convertRemToPixels(3);
 
 
 const style: CSSProperties = {
-    height: windowHeight + 'px'
-  }
-
-function convertRemToPixels(rem: number) {    
-    return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+  height: windowHeight + 'px'
 }
 
-// ------------------
-
-
-interface IState {
-    issue?: any; 
-  }
-
-
-
+function convertRemToPixels(rem: number) {
+  return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
 
 export const Scrollview: FC<{}> = () => {
 
-  
   // let articles = null; 
-  const [articles, setArticles] =  useState<Article[]>([]);
-  
+  const [articles, setArticles] = useState<Article[]>([]);
+
   useEffect(() => {
     getIssue(1, LANGUAGES.EN, (data) => {
-        console.log('current', data)
-        // set state ...
-        
-        setArticles(data.articles);
+      console.log('current', data)
+      // set state ...
+      setArticles(data.articles);
 
-      });
-    
-  },[]);
+    });
 
+  }, []);
 
+  return (
+    <div className="flex flex-row overflow-x-scroll overflow-y-hidden w-auto h-full scroll-snap" style={style}>
 
-    return (
-        <div className="flex flex-row overflow-x-scroll overflow-y-hidden w-auto h-full scroll-snap" style={style}>
+      { articles.map(item =>
+        <Pageview key={item.articleId} article={{
+          id: item.position,
+          date: `CONVERT DATE HERE`,
+          image: item.imageUrl,
+          image_credit: item.credit,
+          link: item.url,
+          headline: item.title,
+          provider: item.source,
+          description: item.teaser
+        }} />
+      )}
 
-            { articles.map( item => 
-              <Pageview key={ item.articleId } article={{
-                id: item.position, 
-                date: `CONVERT DATE HERE`, 
-                image: item.imageUrl, 
-                image_credit: item.credit, 
-                link: item.url, 
-                headline: item.title, 
-                provider: item.source, 
-                description: item.teaser
-                }}/>
-              ) }
-
-        </div>
+    </div>
   );
 }
